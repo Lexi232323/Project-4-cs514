e736280e246ce19ea0071731ced1800baeeb2c7a
lexilin <xlin38@dons.usfca.edu>
2024-11-07 12:53:09 -0800

    submission

--- WheelOfFortunePlayer.java
+++
@@ -1,8 +0,0 @@
-public interface WheelOfFortunePlayer {
-    //get the next guess from the player
-    char nextGuess(String previousGuesses);
-    //an id for the player
-    String playerId();
-    //reset the player to start a new game
-    void reset();
-}
